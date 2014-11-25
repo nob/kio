1d006f273ff66854e5147a7c1c05f7d141e55682
N. Yanbe <yanbe.n@jointanet.com>
2014-11-26 03:23:00 +0900

    layout, style, color modification.

--- htdoc/index.php
+++ htdoc/index.php
@@ -20,7 +20,7 @@
 
     <title>KYOTO SEN'I</title>
     <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
-    <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,300,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/normalize.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/supersized.css" type="text/css" media="screen" />
     <link rel="stylesheet" href="css/supersized.shutter.css" type="text/css" media="screen" />
@@ -87,7 +87,7 @@ if (DEBUG === true) {
     </div>
     <!-- Sliding Panels -->
     <div id="company-pane" class="pane">
-        <div herf="" class="pane-close"></div>
+        <div herf="" class="pane-close"><div></div></div>
         <div class="content">
             <!-- <h2>About US</h2> -->
             <h2>Message from KYOTO SEN'I</h2>
@@ -98,19 +98,21 @@ if (DEBUG === true) {
                 <p>We passionately are working to create an entirely new value of Japanese style and clothing, while building broad awareness of the "Kyo-Yuzen" dyeing process and "Kyo-Shishu" embroidery style in the context of modern lifestyles across the world.</p>
             </div>
             <h3>KYOTO SEN'I CO.,LTD.</h3>
-            <div class="info">
-                <div><h4>Phone</h4><span>+81-75-591-3354</span></div>
-                <div><h4>Fax</h4><span>+81-75-581-8544</span></div>
-            </div>
-            <div class="info">
-                <h4>Address</h4>
-                <p>
-                    42 Haranishi-cho Goryo <br/>
-                    Yamashina-ku, Kyoto 607-8424<br/>
-                    Japan&nbsp;&nbsp;
-                    <a href="map.html" id="open-map" class="button" rel="#map">map</a>
-                </p>
-            </div>
+            <dl class="info">
+              <dt>Address</dt><dd>42 Haranishi-cho Goryo Yamashina-ku, Kyoto 607-8424, Japan&nbsp;&nbsp;<a href="map.html" id="open-map" class="button" rel="#map">map</a></dd>
+              <dt>Phone</dt><dd>+81-75-591-3354</dd>
+              <dt>Fax</dt><dd>+81-75-581-8544</dd>
+              <dt>President</dt><dd>ki Kitamura</dd>
+              <dt>Establishment</dt><dd>1964</dd>
+              <dt>Founded</dt><dd>1970</dd>
+              <dt>Capital</dt><dd>25,000,000 yen</dd>
+              <dt>Bank</dt><dd>THE BANK OF TOKYO-MITSUBISHI UFJ, LTD. KYOTO-CHUO BRANCH NO. 1457910<br>
+                     RESONA BANK, LTD. KYOTO BRANCH NO.1648737<br>
+                     THE KYOTO CHUO SHINKIN BANK MISASAGI BRANCH NO.5021564</dd>
+              <dt>Products</dt><dd>Kimono, Yukata, Happi, T-shirt which are designed for foreign trade,<br>
+                         traditional handy crafts. Japanese Ornaments, variety items</dd>
+              <dt>Business outline</dt><dd>manufacture, wholesale, export</dd>
+            </dl>
         </div>
         <div class="content ja">
             <!-- <h2>About US</h2> -->
@@ -121,49 +123,46 @@ if (DEBUG === true) {
                 <p>日本の伝統文化と世界に誇れる美しさを現代のライフスタイルの中に活かし、京友禅・京刺繍を更に世界中に広めていければ、こんなに嬉しい事はありません。</p>
             </div>
             <h3>株式会社　京都繊維</h3>
-            <div class="info">
-                <div><h4>電話</h4><span>075-591-3354</span></div>
-                <div><h4>Fax</h4><span>075-581-8544</span></div>
-            </div>
-            <div class="info">
-                <h4>所在地</h4>
-                <p>
-                    〒607-8424 <br/>京都市山科区御陵原西町42番地<br/>
-                    <a href="map.html" id="open-map-ja" class="button" rel="#map">地図</a>
-                </p>
+            <dl class="info">
+              <dt>代表取締役</dt><dd>北村繁明</dd>
+              <dt>所在地</dt><dd>〒607-8424 京都市山科区御陵原西町42番地&nbsp;&nbsp;<a href="map.html" id="open-map-ja" class="button" rel="#map">地図</a></dd>
+              <dt>電話</dt><dd>075-591-3354</dd>
+              <dt>Fax</dt><dd>075-581-8544</dd>
+              <dt>創業</dt><dd>1964年</dd>
+              <dt>会社設立</dt><dd>1970年</dd>
+              <dt>資本金</dt><dd>2,500万円</dd>
+              <dt>取引銀行</dt><dd>三菱東京UFJ銀行京都中央支店・りそな銀行京都支店・京都中央信用金庫御陵支店</dd>
+              <dt>取扱商品</dt><dd>外国人向け繊維製品 (着物、浴衣、伴天、Tシャツ、その他繊維製品)、和雑貨、伝統工芸品等</dd>
+              <dt>事業内容</dt><dd>製造、卸、輸出</dd>
+            </dl>
             </div>
         </div>
     </div>
     <div id="products-pane" class="pane">
-        <div class="pane-close"></div>
+        <div class="pane-close"><div></div></div>
         <div class="content">
             <h2>Product Catalogs</h2>
             <ul id="catalogs">
                 <li>
-                    <a href="catalog/kimono+yukata+happi_v10.pdf" rel="#pdf-1">Kimono, Yukata & Happi</a>
-                    <a href="catalog/kimono+yukata+happi_v10.pdf" rel="#pdf-4" class="ja">着物、浴衣、半被</a>
+                    <a href="catalog/wafu_bags_v10.pdf" rel="#pdf-1">Kimono, Yukata & Happi<br><span class="ja">着物、浴衣、半被</span></a>
                 </li>
                 <li>
-                    <a href="catalog/kids-yukata+jinbei_v10.pdf" rel="#pdf-2">Kid's Yukata & Jinbei</a>
-                    <a href="catalog/kids-yukata+jinbei_v10.pdf" rel="#pdf-5" class="ja">子供用 浴衣、甚平</a>
+                    <a href="catalog/kids-yukata+jinbei_v10.pdf" rel="#pdf-2">Kid's Yukata & Jinbei<br><span class="ja">子供用 浴衣、甚平</span></a>
                 </li>
                 <li>
-                    <a href="catalog/obi+kinran_v10.pdf" rel="#pdf-3">Obi & Kinran</a>
-                    <a href="catalog/obi+kinran_v10.pdf" rel="#pdf-6" class="ja">帯、金襴</a>
+                    <a href="catalog/obi+kinran_v10.pdf" rel="#pdf-3">Obi & Kinran<br><span class="ja">帯、金襴</span></a>
                 </li>
             </ul>
         </div>
     </div>
     <div id="inquiry-pane" class="pane">
-        <div class="pane-close"></div>
+        <div class="pane-close"><div></div></div>
         <div class="content">
             <h2>Inquiries</h2>
-            <h3>If you have any inquiries relating to your order or any other aspect of our products, please contact us.</h3>
-            <a class="mailto" href="mailto:kyo-sen@kyosen-gift.co.jp">kyo-sen@kyosen-gift.co.jp</a>
-        </div>
-        <div class="content ja">
-            <h2>お問い合わせ</h2>
-            <h3>ご注文や製品に関するお問い合わせはこちらからご連絡下さい。</h3>
+            <h3>If you have any inquiries relating to your order or any other aspect of our products, <br>please contact us.</h3>
+            <div class="content ja">
+                <h3>ご注文や製品に関するお問い合わせはこちらからご連絡下さい。</h3>
+            </div>
             <a class="mailto" href="mailto:kyo-sen@kyosen-gift.co.jp">kyo-sen@kyosen-gift.co.jp</a>
         </div>
             <!-- <hr/> -->
@@ -216,33 +215,6 @@ if (DEBUG === true) {
             <iframe class="pdf-frame" src="" frameborder="0" scrolling="no"></iframe>
             <div class="footer">
                 <a href="" target="_blank" class="button download">Download Hi-Quality Catalog</a>
-            </div>
-        </div>
-    </div>
-    <div id="pdf-4" class="overlay pdf-overlay ja">
-        <a class="close"></a>
-        <div class="wrap-overlay">
-            <iframe class="pdf-frame" src="" frameborder="0" scrolling="no"></iframe>
-            <div class="footer">
-                <a href="" target="_blank" class="button download">高画質カタログ ダウンロード</a>
-            </div>
-        </div>
-    </div>
-    <div id="pdf-5" class="overlay pdf-overlay ja">
-        <a class="close"></a>
-        <div class="wrap-overlay">
-            <iframe class="pdf-frame" src="" frameborder="0" scrolling="no"></iframe>
-            <div class="footer">
-                <a href="" target="_blank" class="button download">高画質カタログ ダウンロード</a>
-            </div>
-        </div>
-    </div>
-    <div id="pdf-6" class="overlay pdf-overlay ja">
-        <a class="close"></a>
-        <div class="wrap-overlay">
-            <iframe class="pdf-frame" src="" frameborder="0" scrolling="no"></iframe>
-            <div class="footer">
-                <a href="" target="_blank" class="button download">高画質カタログ ダウンロード</a>
             </div>
         </div>
     </div>
